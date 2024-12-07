d7ebb570002c8c8b2afa15677318ecce402cab67
the-vesuvius <steven.stas92@gmail.com>
2024-12-07 20:52:37 +0200

    day7.1

---
+++ day07/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, _ := os.Open("./input.txt")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	table := [][]int{}
+	for scanner.Scan() {
+		res := []int{}
+		row := scanner.Text()
+		parts := strings.Split(row, ":")
+		num, _ := strconv.Atoi(parts[0])
+		res = append(res, num)
+		numbers := strings.SplitN(strings.TrimSpace(parts[1]), " ", -1)
+		for _, numStr := range numbers {
+			numStr = strings.TrimSpace(numStr)
+			tmpNum, _ := strconv.Atoi(numStr)
+			res = append(res, tmpNum)
+		}
+
+		table = append(table, res)
+	}
+
+	sum := 0
+	for _, row := range table {
+		if isValid(row) {
+			sum += row[0]
+		}
+	}
+
+	fmt.Println(sum)
+}
+
+func isValid(row []int) bool {
+	expectedResult := row[0]
+	numbers := row[1:]
+
+	err1 := step(expectedResult, numbers[0], numbers[1:], "+")
+	err2 := step(expectedResult, numbers[0], numbers[1:], "*")
+
+	return err1 == nil || err2 == nil
+}
+
+func step(expected int, resultSoFar int, numbers []int, operator string) error {
+	if len(numbers) == 0 {
+		if expected == resultSoFar {
+			return nil
+		}
+		return errors.New("oh no")
+	}
+	switch operator {
+	case "+":
+		resultSoFar = resultSoFar + numbers[0]
+	case "*":
+		resultSoFar = resultSoFar * numbers[0]
+	}
+
+	err1 := step(expected, resultSoFar, numbers[1:], "+")
+	err2 := step(expected, resultSoFar, numbers[1:], "*")
+
+	if err1 == nil || err2 == nil {
+		return nil
+	}
+
+	return errors.New("oh no")
+}
